eed1b65a4e897ec59e50e2e0351c5fdb24216ba1
faizan542 <faizansilat100@gmail.com>
2024-02-05 18:03:43 +0500

    js file

---
+++ video-96/models/Todo.js
@@ -0,0 +1,12 @@
+// Creating a Schema for DataBase
+import mongoose from "mongoose";
+
+
+const TodoSchema = new mongoose.Schema({
+    title: String,
+    desc: String,
+    isDone: Boolean,
+    days: Number
+});
+
+export const Todo = mongoose.model("Todo", TodoSchema)
